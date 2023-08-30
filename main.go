4da77a52fe9df30f4afe970d1564ff5d64fbb62c
bartick <bartickmaiti1998@gmail.com>
2023-08-30 22:26:20 +0530

    fix: changed how cache dir works

--- main.go
+++ main.go
@@ -41,14 +41,6 @@ func ProxyHandler(hosts *HostMap) func(http.ResponseWriter, *http.Request) {
 	}
 }
 
-func cacheDir() string {
-	home, err := os.UserHomeDir()
-	if err != nil {
-		panic(err)
-	}
-	return home + "/.cache/certs"
-}
-
 func runServer(hosts *HostMap, handler http.Handler) {
 
 	hostMap := hosts.GetHostsArray()
@@ -58,7 +50,7 @@ func runServer(hosts *HostMap, handler http.Handler) {
 		certManager := autocert.Manager{
 			Prompt:     autocert.AcceptTOS,
 			HostPolicy: autocert.HostWhitelist(hostMap...), //Your domain here
-			Cache:      autocert.DirCache(cacheDir()),      //Folder for storing certificates
+			Cache:      autocert.DirCache("certs"),         //Folder for storing certificates
 		}
 
 		server := http.Server{
